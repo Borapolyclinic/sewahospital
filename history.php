753b4fc8d339b674ddc970c07da6659dcec5b07a
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-01-24 10:21:44 +0530

    first commit

---
+++ history.php
@@ -0,0 +1,5 @@
+<?php
+$title = "History | Sewa Hospital & Research Center";
+include('includes/header.php') ?>
+<?php include('includes/navbar.php') ?>
+<?php include('includes/footer.php') ?>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+$title = "Sewa Hospital & Research Center";
+include('includes/header.php') ?>
+<?php include('includes/navbar.php') ?>
+<?php include('includes/footer.php') ?>
