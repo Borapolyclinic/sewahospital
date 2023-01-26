be8b322513bf6828e372f551fec8da970db149f6
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-01-26 13:29:53 +0530

    modified:   assets/css/styles.css new file:   assets/images/webp/banner-img-1.webp new file:   components/home/section-1.php modified:   includes/navbar.php modified:   index.php

---
+++ components/home/section-1.php
@@ -0,0 +1,11 @@
+<div class="home-section-1">
+    <div class="col-md-6 home-section-1-txt">
+        <p class="home-section-1-caption">Sewa Super Speciality Hospital</p>
+        <h1>A <span>LEGACY</span> OF EXCELLENCE</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores esse iste cum.</p>
+        <a href="#" class="home-section-1-button">Know More</a>
+    </div>
+    <div class="col-md-6 home-section-1-img">
+        <img src="assets/images/webp/banner-img-1.webp" alt="">
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
-// $title = "Sewa Hospital & Research Center";
-// include('includes/header.php') 
+$title = "Sewa Hospital & Research Center";
+include('includes/header.php')
 ?>
 <?php
-$title = "Under Maintenance | Sewa Hospital & Research Center";
-include('includes/header.php') ?>
-<?php include('includes/error-404.php') ?>
-<?php // include('includes/navbar.php') 
+// $title = "Under Maintenance | Sewa Hospital & Research Center";
+// include('includes/header.php') 
 ?>
-<?php // include('includes/footer.php') 
+<?php // include('includes/error-404.php') 
+?>
+<?php include('includes/navbar.php')
+?>
+<?php include('components/home/section-1.php') ?>
+<?php include('includes/footer.php')
 ?>
