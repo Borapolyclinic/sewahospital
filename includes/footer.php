2ba19b6c96f6f2851cc39e31880b743e880b7142
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-01-27 13:55:13 +0530

    modified:   assets/css/styles.css modified:   includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,50 @@
+<!-- =============== FOOTER =============== -->
+<div class="footer-menu">
+    <div class="menu-list">
+        <img src="assets/images/nav-logo.webp" alt="">
+        <ul class="footer-nav">
+            <li>
+                <p class="footer-label">Address</p>
+                <a href="https://goo.gl/maps/WoVYx3gSTtsX1sA48" target="_blank" rel="noopener noreferrer">Sewa Nagar,
+                    NH-24, Sitapur Rd, Mubarakpur, Lucknow,
+                    Uttar Pradesh 226201</a>
+            </li>
+
+            <li>
+                <p class="footer-label">Contact</p>
+                <a href="tel:+919876543210">+91 9876543210</a>
+            </li>
+
+            <li>
+                <p class="footer-label">Email</p>
+                <a href="mailto:abc@xyz.com">abc@xyz.com</a>
+            </li>
+        </ul>
+    </div>
+    <div class="menu-list">
+        <ul class="footer-nav">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Who we are</a></li>
+            <li><a href="#">Our History</a></li>
+            <li><a href="#">Events</a></li>
+            <li><a href="#">Connect With Us</a></li>
+        </ul>
+    </div>
+    <div class="menu-list">
+        <ul class="footer-nav">
+            <li><a href="#">Terms & Conditions</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Disclaimer</a></li>
+        </ul>
+    </div>
+</div>
+
+<!-- =============== DEVELOPER FOOTER =============== -->
+<div class="developer-footer">
+    <p>&#169; All Rights Reserved | Designed & Developed by <a href="https://onlynus.com" target="_blank"
+            rel="noopener noreferrer">Onlyn</a></p>
+</div>
+
 <!-- =============== BOOTSTRAP =============== -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
