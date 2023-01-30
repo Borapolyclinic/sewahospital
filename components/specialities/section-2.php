15c4fef43acfe17af94aec80cf9bb34a097c3371
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-01-30 15:42:46 +0530

    modified:   assets/css/styles.css modified:   components/specialities/section-2.php

--- components/specialities/section-2.php
+++ components/specialities/section-2.php
@@ -9,20 +9,59 @@
             <img src="assets/images/webp/element-1.webp" alt="">
         </div>
         <div class="col-md-6 speciality-section-2-txt">
-            <ul>
-                <li>HDU & Analysis</li>
-                <li>Digital X-Ray, NABL Accredited Pathology Laboratory</li>
-                <li>Separate Air Conditioned Male & Female Wards</li>
-                <li>Semi Private, Private & Deluxe Rooms</li>
-                <li>24X7 Pharmacy</li>
-                <li>Regular OPD's</li>
-                <li>Quality policy & Quality manual based on relevant Indian standards</li>
-                <li>All Goverment state and center approvals, norms and procedures in place</li>
-                <li>Qualified, trained, Courteous Paramedics & Medical Staff Supprt</li>
-                <li>Experienced Team of Specialists & Super Specialist Clinicians & Doctors</li>
-                <li>Abreast with the latest development in the field of Medical Science and technology</li>
-                <li>24X7 Ambulance Service</li>
-            </ul>
+            <div class="speciality-section-2-pill">
+                <p>HDU & Analysis</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>Digital X-Ray, NABL Accredited Pathology Laboratory</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>Separate Air Conditioned Male & Female Wards</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>Semi Private, Private & Deluxe Rooms</p>
+            </div>
+
+            <div class="speciality-section-2-pill">
+                <p>Quality policy & Quality manual based on relevant Indian standards</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>All Goverment state and center approvals, norms and procedures in place</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>Qualified, trained, Courteous Paramedics & Medical Staff Supprt</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>Experienced Team of Specialists & Super Specialist Clinicians & Doctors</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>Abreast with the latest development in the field of Medical Science and technology</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>24X7 Ambulance Service</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>24X7 Pharmacy</p>
+            </div>
+            <div class="speciality-section-2-pill">
+                <p>Regular OPD's</p>
+            </div>
+            <!-- <ul>
+                <li>
+                    <div class="inner-li"></div>
+                </li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul> -->
         </div>
     </div>
 </div>
