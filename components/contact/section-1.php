ae01bd1135bae46aef042c16decdfd32700ac658
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-02-16 11:36:56 +0530

    modified:   assets/css/styles.css new file:   components/contact/section-1.php modified:   contact.php

---
+++ components/contact/section-1.php
@@ -0,0 +1,39 @@
+<div class="container mt-5">
+    <div class=" section-header">
+        <h2>Connect <span>with</span> us</h2>
+        <p>Tell us, how can we help?</p>
+    </div>
+
+    <div class="contact-section-1-row">
+        <div class="col-md-6 contact-section-1-graphic">
+            <lottie-player src="https://assets10.lottiefiles.com/packages/lf20_gaplvsns.json" background="transparent"
+                speed="1" loop autoplay></lottie-player>
+        </div>
+
+        <form action="" method="POST" class="contact-section-form col-md-6">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="contactName" placeholder="Full Name">
+                <label for="contactName">Your Name</label>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="number" class="form-control" id="contactNumber" placeholder="Contact number">
+                <label for="contactNumber">+91 XXXXX XXXXX</label>
+            </div>
+
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="contactEmail" placeholder="Contact number">
+                <label for="contactEmail">Email Address</label>
+            </div>
+            <div class="form-floating mb-3">
+                <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2"
+                    style="height: 100px"></textarea>
+                <label for="floatingTextarea2">Comments</label>
+            </div>
+
+            <button class="contact-section-1-btn">Submit</button>
+        </form>
+
+    </div>
+
+
+</div>
--- contact.php
+++ contact.php
@@ -2,4 +2,5 @@
 $title = "Connect with us | Sewa Hospital & Research Center";
 include('includes/header.php') ?>
 <?php include('includes/navbar.php') ?>
+<?php include('components/contact/section-1.php') ?>
 <?php include('includes/footer.php') ?>
