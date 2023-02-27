28e7f51a38c2dbfd3e077f36c7449452a8becfa3
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-02-27 14:51:44 +0530

    new file:   assets/images/icon/care.webp new file:   assets/images/icon/cost-effective.webp new file:   assets/images/icon/state-of-art.webp new file:   assets/images/icon/trust.webp modified:   components/who-we-are/section-2.php

--- components/who-we-are/section-2.php
+++ components/who-we-are/section-2.php
@@ -7,7 +7,8 @@
 
     <div class="who-we-are-section-2-row">
         <div class="col-md-6 who-we-are-section-2-card">
-            <img src="assets/images/temp/icon-1.png" alt="">
+            <img src="assets/images/icon/cost-effective.webp"
+                alt="Sewa Hospital & Research Centre's Cost-effective medical care Icon">
             <div class="who-we-are-section-2-card-inner-text">
                 <h3>Cost-effective medical care</h3>
                 <p>We provide free consultation to patients in our Weekly Free Clinics like Diabetes Clinic, Preventive
@@ -17,7 +18,8 @@
         </div>
 
         <div class="col-md-6 who-we-are-section-2-card">
-            <img src="assets/images/temp/icon-1.png" alt="">
+            <img src="assets/images/icon/state-of-art.webp"
+                alt="Sewa Hospital & Research Centre's State of the Art Technology Icon">
             <div class="who-we-are-section-2-card-inner-text">
                 <h3>State of the Art Technology</h3>
                 <p>We are equipped with the latest state of the art Technology equipment’s to perform procedures and
@@ -28,7 +30,8 @@
 
     <div class="who-we-are-section-2-row">
         <div class="col-md-6 who-we-are-section-2-card">
-            <img src="assets/images/temp/icon-1.png" alt="">
+            <img src="assets/images/icon/care.webp"
+                alt="Sewa Hospital & Research Centre's Comprehensive Care Delivery Model Icon">
             <div class="who-we-are-section-2-card-inner-text">
                 <h3>Comprehensive Care Delivery Model</h3>
                 <p>We deliver comprehensive care in over 10+ specialities by adopting a service-oriented delivery model,
@@ -38,7 +41,7 @@
         </div>
 
         <div class="col-md-6 who-we-are-section-2-card">
-            <img src="assets/images/temp/icon-1.png" alt="">
+            <img src="assets/images/icon/trust.webp" alt="Sewa Hospital & Research Centre's A trusted brand name Icon">
             <div class="who-we-are-section-2-card-inner-text">
                 <h3>A trusted brand name</h3>
                 <p>With more than 30 years of legacy and a trusted name in the business, we are continuously learning to
