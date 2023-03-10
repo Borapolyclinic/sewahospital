463dfeb81986e663e8bdff0ca7d17e404cad505b
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-03-10 13:46:29 +0530

    modified:   assets/css/styles.css modified:   assets/images/webp/banner-img-1-building.webp modified:   components/home/section-1.php

--- components/home/section-1.php
+++ components/home/section-1.php
@@ -2,14 +2,14 @@
     <div class="carousel-inner">
         <div class="carousel-item active">
             <div class="home-section-1">
+                <div class="col-md-6 home-section-1-img">
+                    <img src="assets/images/webp/banner-img-1-building.webp" alt="Home page banner for Sewa Hospital">
+                </div>
                 <div class="col-md-6 home-section-1-txt">
                     <p class="home-section-1-caption">A Unit of Bora Polyclinic</p>
                     <h1>A <span>SUPER SPECIALITY</span> HOSPITAL</h1>
                     <p>NABH Accredited Lab ISO 15189:2012 | NABH Certified Hospital (Entry Level)</p>
                     <a href="who-we-are.php" class="home-section-1-button">Know More</a>
-                </div>
-                <div class="col-md-6 home-section-1-img">
-                    <img src="assets/images/webp/banner-img-1-building.webp" alt="Home page banner for Sewa Hospital">
                 </div>
             </div>
         </div>
