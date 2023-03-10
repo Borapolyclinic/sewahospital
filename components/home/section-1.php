42ddb95d7efffee1eee4c04925378f1c41336fb1
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-03-10 13:28:18 +0530

    deleted:    .htaccess new file:   assets/images/webp/banner-img-1-building.webp modified:   components/home/section-1.php

--- components/home/section-1.php
+++ components/home/section-1.php
@@ -9,7 +9,7 @@
                     <a href="who-we-are.php" class="home-section-1-button">Know More</a>
                 </div>
                 <div class="col-md-6 home-section-1-img">
-                    <img src="assets/images/webp/banner-img-1.webp" alt="Home page banner for Sewa Hospital">
+                    <img src="assets/images/webp/banner-img-1-building.webp" alt="Home page banner for Sewa Hospital">
                 </div>
             </div>
         </div>
