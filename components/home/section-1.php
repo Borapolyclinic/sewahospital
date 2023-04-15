21f5ddfc8b1f533d99e01d7a13dd8e2a1510ab9c
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-04-15 09:41:31 +0530

    modified:   assets/css/styles.css new file:   assets/images/webp/home-banner-2.webp modified:   components/home/section-1.php

--- components/home/section-1.php
+++ components/home/section-1.php
@@ -2,6 +2,13 @@
     <div class="carousel-inner">
         <div class="carousel-item active">
             <div class="home-section-1">
+                <a href="specialities.php#insurance-&-tpa" class="col-md-6 home-section-1-img-banner-2">
+                    <img src="assets/images/webp/home-banner-2.webp" alt="Home page banner for Sewa Hospital">
+                </a>
+            </div>
+        </div>
+        <div class="carousel-item ">
+            <div class="home-section-1">
                 <div class="col-md-6 home-section-1-img">
                     <img src="assets/images/webp/banner-img-1-building.webp" alt="Home page banner for Sewa Hospital">
                 </div>
@@ -18,38 +25,15 @@
                 </div>
             </div>
         </div>
-        <!-- <div class="carousel-item">
-            <div class="home-section-1">
-                <div class="col-md-6 home-section-1-txt">
-                    <p class="home-section-1-caption">A Unit of Bora Polyclinic</p>
-                    <h1>A <span>SUPER SPECIALITY</span> HOSPITAL</h1>
-                    <p>NABH Accredited Lab ISO 15189:2012 | NABH Certified Hospital (Entry Level)</p>
-                    <a href="who-we-are.php" class="home-section-1-button">Know More</a>
-                </div>
-                <div class="col-md-6 home-section-1-img">
-                    <img src="assets/images/webp/banner-img-1.webp" alt="">
-                </div>
-            </div>
-        </div> -->
-        <!-- <div class="carousel-item">
-            <div class="home-section-1">
-                <div class="col-md-6 home-section-1-txt">
-                    <p class="home-section-1-caption">A Unit of Bora Polyclinic</p>
-                    <h1>A <span>SUPER SPECIALITY</span> HOSPITAL</h1>
-                    <p>NABH Accredited Lab ISO 15189:2012 | NABH Certified Hospital (Entry Level)</p>
-                    <a href="who-we-are.php" class="home-section-1-button">Know More</a>
-                </div>
-                <div class="col-md-6 home-section-1-img">
-                    <img src="assets/images/webp/banner-img-1.webp" alt="">
-                </div>
-            </div>
-        </div> -->
+
     </div>
-    <button class="carousel-control-prev" id="homepage-banner-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <button class="carousel-control-prev" id="homepage-banner-prev" type="button" data-bs-target="#carouselExample"
+        data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Previous</span>
     </button>
-    <button class="carousel-control-next" id="homepage-banner-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <button class="carousel-control-next" id="homepage-banner-next" type="button" data-bs-target="#carouselExample"
+        data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
     </button>
