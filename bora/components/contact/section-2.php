3bd1610f36a50b21dc8351093415c47ab0c14201
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-04-17 13:32:12 +0530

    modified:   bora/components/contact/section-2.php

--- bora/components/contact/section-2.php
+++ bora/components/contact/section-2.php
@@ -31,35 +31,73 @@
         </div>
     </div>
 
-    <form action="" method="POST" class="col-md-6 contact-section-2-form">
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Full Name</label>
-            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Your Name">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Contact Number</label>
-            <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="XXXXX XXXXX">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Contact About?</label>
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Open this menu</option>
-                <option value="1">Courses</option>
-                <option value="2">Admissions</option>
-                <option value="3">Fee</option>
-                <option value="4">Career</option>
-                <option value="5">Other</option>
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Details</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
+    <div class="col-md-6">
+        <?php
+        include('include/db.php');
+        if (isset($_POST['submit'])) {
+            $contact_name = mysqli_real_escape_string($connection, $_POST['contact_name']);
+            $contact_number = mysqli_real_escape_string($connection, $_POST['contact_number']);
+            $contact_email = mysqli_real_escape_string($connection, $_POST['contact_email']);
+            $contact_reason = mysqli_real_escape_string($connection, $_POST['contact_reason']);
+            $contact_details = mysqli_real_escape_string($connection, $_POST['contact_details']);
 
-        <button class="contact-section-2-btn">Submit</button>
-    </form>
+            $query = "INSERT INTO `bora_contact`(
+                `contact_name`,
+                `contact_number`,
+                `contact_email`,
+                `contact_reason`,
+                `contact_details`
+            )
+            VALUES(
+                '$contact_name',
+                '$contact_number',
+                '$contact_email',
+                '$contact_reason',
+                '$contact_details'
+            )";
+            $result = mysqli_query($connection, $query);
+
+            if ($result) { ?>
+        <div class="alert alert-success mt-3 mb-3" role="alert">Thank you for contact us. We will connect with you
+            shortly.</div>
+        <?php
+            }
+        }
+        ?>
+        <form action="" method="POST" class="contact-section-2-form">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+                <input type="text" name="contact_name" class="form-control" id="exampleFormControlInput1"
+                    placeholder="Your Name" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Contact Number</label>
+                <input type="number" name="contact_number" class="form-control" id="exampleFormControlInput1"
+                    placeholder="XXXXX XXXXX" required>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                <input type="email" name="contact_email" class="form-control" id="exampleFormControlInput1"
+                    placeholder="name@example.com">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Contact About?</label>
+                <select class="form-select" name="contact_reason" aria-label="Default select example" required>
+                    <option selected>Open this menu</option>
+                    <option value="1">Courses</option>
+                    <option value="2">Admissions</option>
+                    <option value="3">Fee</option>
+                    <option value="4">Career</option>
+                    <option value="5">Other</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Details</label>
+                <textarea class="form-control" name="contact_details" id="exampleFormControlTextarea1"
+                    rows="3"></textarea>
+            </div>
+
+            <button type="submit" name="submit" class="contact-section-2-btn">Submit</button>
+        </form>
+    </div>
 </div>
