6da93ded8a96c80e3e1493fb7a889c5f0e8e1bba
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-04-30 09:40:27 +0530

    modified:   includes/header.php

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,25 @@
 <html lang="en">
 
 <head>
+    <!-- =============== GOOGLE TAG MANAGER =============== -->
+    <script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-THL8N6X');
+    </script>
+
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -46,6 +65,11 @@
 </head>
 
 <body>
+
+    <!-- ========== GOOGLE TAG MANAGER ========== -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THL8N6X" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+
     <?php include('includes/cookie.php') ?>
     <button onclick="topFunction()" id="myBtn" title="Go to top">
         <ion-icon name="caret-up-circle-outline"></ion-icon>
