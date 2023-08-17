b81a019593ecac668ce8daff313237d7a44dcc08
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2023-08-17 10:37:55 +0530

    modified:   includes/header.php

--- includes/header.php
+++ includes/header.php
@@ -6,32 +6,32 @@
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-084JPKW7K7"></script>
     <script>
-        window.dataLayer = window.dataLayer || [];
+    window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
 
-        gtag('config', 'G-084JPKW7K7');
+    gtag('config', 'G-084JPKW7K7');
     </script>
 
     <!-- =============== GOOGLE TAG MANAGER =============== -->
     <script>
-        (function(w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({
-                'gtm.start': new Date().getTime(),
-                event: 'gtm.js'
-            });
-            var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src =
-                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-THL8N6X');
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-THL8N6X');
     </script>
 
 
@@ -40,14 +40,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- =============== SEO =============== -->
-    <meta name="keywords" content="Sewa Hospital Lucknow, Research Centre in Lucknow, Best Hospital in Lucknow, Top Hospitals in Lucknow, Healthcare in Lucknow, Medical Services in Lucknow, Sewa Hospital & Research Centre Lucknow, Speciality Services in Lucknow, Advanced Medical Care in Lucknow, Emergency Medical Care in Lucknow, Affordable Healthcare in Lucknow, State-of-the-art Hospital in Lucknow, Best Healthcare Services in Lucknow, Experienced Doctors in Lucknow, Leading Medical Centre in Lucknow, Medical Treatment in Lucknow, Quality Healthcare in Lucknow, Patient Care in Lucknow.">
-    <meta name='description' content='Experience the best in healthcare at Sewa Hospital & Research Centre Lucknow. Advanced medical care, specialty services, and experienced doctors for quality patient care.'>
+    <meta name="keywords"
+        content="Sewa Hospital Lucknow, Research Centre in Lucknow, Best Hospital in Lucknow, Top Hospitals in Lucknow, Healthcare in Lucknow, Medical Services in Lucknow, Sewa Hospital & Research Centre Lucknow, Speciality Services in Lucknow, Advanced Medical Care in Lucknow, Emergency Medical Care in Lucknow, Affordable Healthcare in Lucknow, State-of-the-art Hospital in Lucknow, Best Healthcare Services in Lucknow, Experienced Doctors in Lucknow, Leading Medical Centre in Lucknow, Medical Treatment in Lucknow, Quality Healthcare in Lucknow, Patient Care in Lucknow.">
+    <meta name='description'
+        content="Experience top-quality healthcare at Sewa Hospital & Research Centre Lucknow: Specialized services, advanced care, and experienced doctors.">
 
     <!-- =============== TITLE =============== -->
     <title><?php echo $title ?></title>
 
     <!-- =============== BOOTSTRAP =============== -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
     <!-- =========== FAVICON =========== -->
     <link rel="icon" href="assets/images/favicon.ico" sizes="any"><!-- 32×32 -->
@@ -60,7 +63,9 @@
     <!-- =========== GOOGLE FONTS =========== -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=ABeeZee&family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=ABeeZee&family=League+Spartan:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
 
     <!-- =========== LOTTIE FILES =========== -->
     <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
@@ -69,27 +74,29 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 
     <!-- ========== JQUERY ========== -->
-    <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.3.js"
+        integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
 
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11278379147">
     </script>
     <script>
-        window.dataLayer = window.dataLayer || [];
+    window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
 
-        gtag('config', 'AW-11278379147');
+    gtag('config', 'AW-11278379147');
     </script>
 </head>
 
 <body>
     <!-- ========== GOOGLE TAG MANAGER ========== -->
     <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THL8N6X" height="0" width="0" style="display:none;visibility:hidden">
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THL8N6X" height="0" width="0"
+            style="display:none;visibility:hidden">
         </iframe>
     </noscript>
 
